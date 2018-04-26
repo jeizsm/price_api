2a5779a1a41cb9b226acf17df9d189c6a248a1c2
Marat Safin <jeizsm@gmail.com>
2018-04-26 12:15:20 +0300

    use macros for nearest
    Signed-off-by: Marat Safin <jeizsm@gmail.com>

--- lib/price_api/location/caches.ex
+++ lib/price_api/location/caches.ex
@@ -3,42 +3,39 @@ defmodule PriceApi.Location.Caches do
   alias PriceApi.Location.Cache
   alias PriceApi.Repo
 
+  defmacrop near_sphere(field_name, point) do
+    quote do
+      fragment(
+        [{unquote(field_name), [
+          "$nearSphere": [
+            "$geometry": [
+              "type": "Point",
+              "coordinates": ^unquote(point)
+            ],
+            "$maxDistance": 10
+          ]
+        ]}]
+      )
+    end
+  end
+
+  defmacrop get_nearest(field_name, point, time) do
+    quote do
+      from(c in Cache,
+            where: c.inserted_at > ^unquote(time) and nearSphere(unquote(field_name), unquote(point)),
+            order_by: [desc: c.inserted_at],
+            select: [:id, :price])
+    end
+  end
+
   def cached(start_point, end_point) do
     time = NaiveDateTime.add(NaiveDateTime.utc_now(), -5 * 60, :second)
-    %Cache{id: start_id} = Repo.one(get_nearest_start(start_point, time))
-    cache = %Cache{id: end_id} = Repo.one(get_nearest_end(end_point, time))
+    %Cache{id: start_id} = Repo.one(get_nearest(:start, start_point, time))
+    cache = %Cache{id: end_id} = Repo.one(get_nearest(:end, end_point, time))
     if start_id == end_id do
       cache
     else
       nil
     end
-  end
-
-  defp get_nearest_start(point, time) do
-    from(c in Cache, where: c.inserted_at > ^time and fragment(
-      start: [
-        "$nearSphere": [
-          "$geometry": [
-            "type": "Point",
-            "coordinates": ^point
-          ],
-          "$maxDistance": 10
-        ]
-      ]
-    ), order_by: [desc: c.inserted_at], select: [:id, :price])
-  end
-
-  defp get_nearest_end(point, time) do
-    from(c in Cache, where: c.inserted_at > ^time and fragment(
-      end: [
-        "$nearSphere": [
-          "$geometry": [
-            "type": "Point",
-            "coordinates": ^point
-          ],
-          "$maxDistance": 10
-        ]
-      ]
-    ), order_by: [desc: c.inserted_at], select: [:id, :price])
   end
 end
